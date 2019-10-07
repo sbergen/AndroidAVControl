61af4c5ae77e1eee054dfeaa11a98d27d3f11b97
Sakari Bergen <sakari@yousician.com>
2019-10-07 21:02:06 +0300

    Bundle SmartThings commands to one request

--- app/src/main/java/net/beatwaves/homecontrol/SmartThingsDevice.kt
+++ app/src/main/java/net/beatwaves/homecontrol/SmartThingsDevice.kt
@@ -8,41 +8,39 @@ class SmartThingsDevice : Device() {
 
     val powerOnCommand = Command("switch", "on")
     val powerOffCommand = Command("switch", "off")
-    val switchToAnthemInputCommand = inputCommand("HDMI2")
+    val switchToAvmInputCommand = inputCommand("HDMI2")
     val switchToUsbCInputCommand = inputCommand("HDMI1")
 
     override fun powerOff() {
         scope.runAction {
-            sendCommand(powerOffCommand, "power off").await()
+            sendCommand(arrayOf(powerOffCommand), "power off").await()
         }
     }
 
     override fun activateTvMode() {
         scope.runAction {
-            powerOn().await()
+            sendCommand(arrayOf(powerOnCommand), "power on").await()
         }
     }
 
     override fun activateUsbCMode() {
         scope.runAction {
-            powerOn().await()
-            sendCommand(switchToUsbCInputCommand, "switch input").await()
+            val commands = arrayOf(powerOnCommand, switchToUsbCInputCommand)
+            sendCommand(commands, "power on + switch to USB-C").await()
         }
     }
 
     override fun activateDexMode() {
         scope.runAction {
-            powerOn().await()
-            sendCommand(switchToAnthemInputCommand, "switch input").await()
+            val commands = arrayOf(powerOnCommand, switchToAvmInputCommand)
+            sendCommand(commands, "power on + switch to AVM").await()
         }
     }
 
     private fun inputCommand(input: String) = Command("mediaInputSource", "setInputSource", arrayOf(input))
 
-    private suspend fun powerOn() = sendCommand(powerOnCommand, "power on")
-
-    private suspend fun sendCommand(command: Command, description: String): Deferred<Unit> = scope.async {
-        val response = api.executeCommand(Commands(arrayOf(command))).await()
+    private suspend fun sendCommand(commands: Array<Command>, description: String): Deferred<Unit> = scope.async {
+        val response = api.executeCommand(Commands(commands)).await()
         if (!response.isSuccessful || response.body()?.isSuccess == false)
         {
             throw Exception("Failed to $description: ${response.body()?.error ?: response.message()}")
